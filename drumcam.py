00bdf7f7db7e5ebb6c58bf1d7b4d22893d5687bc
Karl-Fredrik Blixt Hagholm <kalle@hagholm.se>
2022-01-06 01:18:55 +0000

    Add python script embryo

---
+++ drumcam.py
@@ -0,0 +1,39 @@
+import getopt, sys
+
+def help():
+    print("drumcam - Drum camera recorder")
+    print("Usage:")
+    print("        drumcam -h")
+    print("        drumcam [options]")
+    print("Options:")
+    print("        -h,--help   Show this help")
+    print("        --name      Set the name of the recording")
+    print("        --take      Set take number")
+    print("        --record    Start recording")
+
+def set_recording_name(name):
+    print("Recording name:", name)
+    
+argument_list = sys.argv[1:]
+
+options = "h"
+long_options = ["help", "name=", "take=", "record="]
+
+try:
+    arguments, values = getopt.getopt(argument_list, options, long_options)
+
+    argument_functions = {"-h": lambda value: help(),
+                          "--help": lambda value: help(),
+                          "--name": lambda value: set_recording_name(value)
+    }
+
+    if len(arguments) == 0:
+        help()
+
+    else:
+        for argument, value in arguments:
+            argument_functions[argument](value)
+        
+except getopt.error as err:
+    print(str(err))
+    help()
